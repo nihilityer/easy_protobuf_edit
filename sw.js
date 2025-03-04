0b401993cb78d5350af7ea538a8f12283af81939
nihilityer <nihilityer@users.noreply.github.com>
2025-03-04 14:49:24 +0000

    Deploying to gh-pages from @ nihilityer/easy_protobuf_edit@e18669f53d964654a274551a6441f8a9edabdd89 🚀

---
+++ sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'epe';
+var filesToCache = [
+  './',
+  './index.html',
+'./easy_protobuf_edit-7882f3fe882ff411.js',
+'./easy_protobuf_edit-7882f3fe882ff411_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
